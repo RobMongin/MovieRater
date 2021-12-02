949369412672588656517233132822fa08f4c31c
Rob Mongin <RobertMongin@outlook.com>
2021-12-02 11:35:12 -0500

    git ignore

--- MovieRater.Models/ContentModels/ContentDetail.cs
+++ MovieRater.Models/ContentModels/ContentDetail.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MovieRater.Models
+namespace MovieRater.Models.ContentModels
 {
-    public class Class1
+    class ContentDetail
     {
     }
 }
