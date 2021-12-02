ff71b929d9931312c4b3ec8aa8a492b64c90b140
Rob Mongin <RobertMongin@outlook.com>
2021-12-02 14:46:23 -0500

    movie prop and foreign key

--- MovieRater.Data/Content.cs
+++ MovieRater.Data/Content.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -50,6 +51,10 @@ namespace MovieRater.Data
 
         [Required]
         public GenreType GenreType { get; set; }
+
+        [ForeignKey(nameof(Movie))]
+        public int MovieId { get; set; }
+        public virtual Movie Movie { get; set; }
         
         public bool IsFamilyFriendly
         {
